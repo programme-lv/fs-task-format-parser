849ccac046987070cabce9dc98f22a3d1804b5d1
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-17 23:12:45 +0300

    add logging to constraints.go

--- pkg/fstaskparser/constraints.go
+++ pkg/fstaskparser/constraints.go
@@ -2,16 +2,24 @@ package fstaskparser
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/pelletier/go-toml/v2"
 )
 
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
 func readCPUTimeLimitInSeconds(specVers string, tomlContent string) (float64, error) {
+	log.Printf("Reading CPU time limit for specification version: %s\n", specVers)
 	cmpres, err := largerOrEqualSemVersionThan(specVers, "2.0")
 	if err != nil {
+		log.Printf("Error comparing semversions: %v\n", err)
 		return 0, fmt.Errorf("error comparing semversions: %w", err)
 	}
 	if !cmpres {
+		log.Printf("Unsupported specification version: %s\n", specVers)
 		return 0, fmt.Errorf("unsupported specification version: %s", specVers)
 	}
 
@@ -25,18 +33,23 @@ func readCPUTimeLimitInSeconds(specVers string, tomlContent string) (float64, er
 
 	err = toml.Unmarshal([]byte(tomlContent), &tomlStruct)
 	if err != nil {
+		log.Printf("Failed to unmarshal the CPU time limit: %v\n", err)
 		return 0, fmt.Errorf("failed to unmarshal the cpu time limit: %w", err)
 	}
 
+	log.Printf("Successfully read CPU time limit: %f seconds\n", tomlStruct.Constraints.CPUTimeLimitInSeconds)
 	return tomlStruct.Constraints.CPUTimeLimitInSeconds, nil
 }
 
 func readMemoryLimitInMegabytes(specVers string, tomlContent string) (int, error) {
+	log.Printf("Reading memory limit for specification version: %s\n", specVers)
 	cmpres, err := largerOrEqualSemVersionThan(specVers, "2.0")
 	if err != nil {
+		log.Printf("Error comparing semversions: %v\n", err)
 		return 0, fmt.Errorf("error comparing semversions: %w", err)
 	}
 	if !cmpres {
+		log.Printf("Unsupported specification version: %s\n", specVers)
 		return 0, fmt.Errorf("unsupported specification version: %s", specVers)
 	}
 
@@ -50,8 +63,10 @@ func readMemoryLimitInMegabytes(specVers string, tomlContent string) (int, error
 
 	err = toml.Unmarshal([]byte(tomlContent), &tomlStruct)
 	if err != nil {
+		log.Printf("Failed to unmarshal the memory limit: %v\n", err)
 		return 0, fmt.Errorf("failed to unmarshal the memory limit: %w", err)
 	}
 
+	log.Printf("Successfully read memory limit: %d megabytes\n", tomlStruct.Constraints.MemoryLimitInMegabytes)
 	return tomlStruct.Constraints.MemoryLimitInMegabytes, nil
 }
